877538449827cee5a3f28a9235b114840020ea53
Fredrik Vraalsen <fredrik@vraalsen.no>
2020-04-10 18:24:23 +0200

    Initial setup of Serverless Flask app demo

--- setup.py
+++ setup.py
@@ -12,10 +12,10 @@ setup(
     version="0.1.0",
     author="Origo Dataplattform",
     author_email="dataplattform@oslo.kommune.no",
-    description="Blueprint for lambda function at Oslo Origo",
+    description="Serverless Flask app demo",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.oslo.kommune.no/origo-dataplatform/lambda-boilerplate",
+    url="https://github.oslo.kommune.no/origo-dataplatform/flask-demo",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=["Flask"],
 )
